00adad746ddbc6b633f53b18abb6252293b9109d
qqdipps <sdipp007@gmail.com>
2019-04-15 15:48:29 -0700

    created controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'passengers/index'
+  get 'passengers/show'
+  get 'passengers/destroy'
+  get 'passengers/new'
+  get 'passengers/create'
+  get 'passengers/edit'
+  get 'passengers/update'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
