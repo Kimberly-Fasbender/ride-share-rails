09f0796a7a40d774b94735e708bd07072acf5a00
qqdipps <sdipp007@gmail.com>
2019-04-15 15:57:08 -0700

    added Restful default routes for passengers

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  get 'passengers/index'
-  get 'passengers/show'
-  get 'passengers/destroy'
-  get 'passengers/new'
-  get 'passengers/create'
-  get 'passengers/edit'
-  get 'passengers/update'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :passengers
 end
