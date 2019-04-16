5605cb94f126ead0b7cd408d35d1010e9a2b5813
qqdipps <sdipp007@gmail.com>
2019-04-15 22:54:07 -0700

    added tests for updating passenger, added view content

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -26,11 +26,30 @@ class PassengersController < ApplicationController
   end
 
   def update
+    id = params[:id]
+    passenger = Passenger.find_by(id: id)
+    if passenger && passenger.update(passenger_params)
+      redirect_to passenger_path(passenger)
+    else 
+      head :not_found
+    end
   end
 
   def create
+    passenger = Passenger.new(passenger_params)
+    unless passenger.save # was getting duplicate key assignment error, ran this command in console ActiveRecord::Base.connection.reset_pk_sequence!('passengers')
+      flash[:error] = "Unable to add Passenger, please try again."
+      head :bad_request # returns 400 then redirects, can I do this?
+    end
+    redirect_to passengers_path
   end
 
   def destroy
+  end
+
+  private
+
+  def passenger_params
+    return params.required(:passenger).permit(:name, :phone_num)
   end
 end
--- test/controllers/passengers_controller_test.rb
+++ test/controllers/passengers_controller_test.rb
@@ -42,7 +42,40 @@ describe PassengersController do
   end
 
   describe "update" do
-    # Your tests go here
+    it "should update valid passenger" do
+      passenger = Passenger.last
+      passenger_hash = {
+        passenger: {
+          name: "Updated Passenger",
+          phone_num: "253-555-update",
+        },
+      }
+      expect {
+        patch passenger_path(passenger.id), params: passenger_hash
+      }.wont_change "Passenger.count"
+
+      must_respond_with :redirect
+      must_redirect_to passenger_path
+
+      passenger.reload
+      expect(passenger.name).must_equal "Updated Passenger"
+      expect(passenger.phone_num).must_equal "253-555-update"
+    end
+
+    it "will return 404 if invalid id is updated" do
+      invalid_id = -1
+      passenger_hash = {
+        passenger: {
+          name: "Update invalidd Passenger",
+          phone_num: "253-555-invalidupdate",
+        },
+      }
+      expect {
+        patch passenger_path(invalid_id), params: passenger_hash
+      }.wont_change "Passenger.count"
+
+      must_respond_with :not_found
+    end
   end
 
   describe "new" do
@@ -65,12 +98,12 @@ describe PassengersController do
         post passengers_path, params: passenger_hash
       }.must_change "Passenger.count", 1
 
-      new_passenger = Passener.last
+      new_passenger = Passenger.last
       expect(new_passenger.name).must_equal "Harry Thumblina"
       expect(new_passenger.phone_num).must_equal "253-555-2609"
     end
 
-    #   it "will be redirected if given invalid params" do  #<-- will need this later I think since trip belongs to a passenger
+    #   it "will be redirected if given invalid params" do  #<-- will need this later ?
     # end
   end
 
