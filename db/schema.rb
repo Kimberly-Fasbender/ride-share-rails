e9526cfa0f2651835954dd426bc5c177d5d7da8c
qqdipps <sdipp007@gmail.com>
2019-04-16 16:55:26 -0700

    migration for available driver

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_16_224540) do
+ActiveRecord::Schema.define(version: 2019_04_16_235351) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2019_04_16_224540) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "vin"
+    t.boolean "available", default: true
   end
 
   create_table "passengers", force: :cascade do |t|
