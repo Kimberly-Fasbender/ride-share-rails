70220e7cf2eadc4c4c549080426a0358ad38a97d
qqdipps <sdipp007@gmail.com>
2019-04-16 15:55:51 -0700

    outline

--- test/controllers/trips_controller_test.rb
+++ test/controllers/trips_controller_test.rb
@@ -1,6 +1,15 @@
 require "test_helper"
 
 describe TripsController do
+  let(:passenger) {
+    Passenger.create(name: "Anubhav Singh", phone_num: "253-555-1474")
+  }
+  let(:driver) {
+    Driver.create(name: "George", vin: "X13873487YVW")
+  }
+  let(:trip) {
+    Trip.create(passenger_id: passenger.id, driver_id: driver.id, date: Date.current, cost: 25.0, rating: 3)
+  }
   describe "index" do
     it "should go to index page" do
       get trips_path
@@ -9,6 +18,12 @@ describe TripsController do
   end
 
   describe "show" do
+    it "should show page for valid trip" do 
+      
+    end
+
+    it "should return 404 for invalid trip id" do 
+    end
   end
 
   describe "edit" do
