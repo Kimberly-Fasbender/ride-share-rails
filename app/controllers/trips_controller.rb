28579a60fa5e8dccc2edbf046250cf12f2c64fce
Kimberly-Fasbender <kimberly.fasbender@gmail.com>
2019-04-16 16:47:58 -0700

    started working on trips controller create action - put on hold

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -4,12 +4,11 @@ class TripsController < ApplicationController
   end
 
   def show
-    id  = params[:id]
+    id = params[:id]
     @trip = Trip.find_by(id: id)
-    unless @trip 
+    unless @trip
       head :not_found
     end
-
   end
 
   def create
@@ -25,5 +24,11 @@ class TripsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def trip_params
+    return params.require(:trip).permit(:cost, :driver_id, :passenger_id, :date, :rating)
   end
 end
