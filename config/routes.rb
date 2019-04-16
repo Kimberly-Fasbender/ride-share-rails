027646a2cd20e16a43b629dd5d8c0114e01df97c
Kimberly-Fasbender <kimberly.fasbender@gmail.com>
2019-04-16 15:56:01 -0700

    added RESTful routes for trips controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
-  get 'trips/index'
-  get 'trips/show'
-  get 'trips/create'
-  get 'trips/new'
-  get 'trips/update'
-  get 'trips/edit'
-  get 'trips/destroy'
+  get "trips/index"
+  get "trips/show"
+  get "trips/create"
+  get "trips/new"
+  get "trips/update"
+  get "trips/edit"
+  get "trips/destroy"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :passengers
 
   resources :drivers
+
+  resources :trips
 end
