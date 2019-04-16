e4d557f45f0328f4d962e75fcad3e6ed0b71ff0f
Kimberly-Fasbender <kimberly.fasbender@gmail.com>
2019-04-16 02:37:59 -0700

    wrote tests for the drivers controller index and show actions

--- test/controllers/drivers_controller_test.rb
+++ test/controllers/drivers_controller_test.rb
@@ -3,12 +3,29 @@ require "test_helper"
 describe DriversController do
   describe "index" do
     it "can get index" do
-      # Your code here
+      get drivers_path
+
+      must_respond_with :success
     end
   end
 
   describe "show" do
-    # Your tests go here
+    it "can show an existing valid driver" do
+      driver = Driver.create(name: "George", vin: "X13873487YVW")
+      valid_id = driver.id
+
+      get driver_path(valid_id)
+
+      must_respond_with :success
+    end
+
+    it "will respond with a 404 with an invalid driver id" do
+      invalid_driver_id = -3
+
+      get driver_path(invalid_driver_id)
+
+      must_respond_with :not_found
+    end
   end
 
   describe "edit" do
