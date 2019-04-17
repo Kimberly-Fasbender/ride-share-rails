e07cbdf099dbf804d55e591c85cfb2a320e462ac
Kimberly-Fasbender <kimberly.fasbender@gmail.com>
2019-04-16 20:14:11 -0700

    added total_earnings method to the driver model

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -4,4 +4,8 @@ class Driver < ApplicationRecord
   def self.next_available
     return Driver.find_by(available: true)
   end
+
+  def total_earnings
+    return self.trips.sum { |trip| trip.cost } / 100
+  end
 end
