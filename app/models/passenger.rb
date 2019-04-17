d3f39aba0237f5cb625298d131566e894b3d1873
qqdipps <sdipp007@gmail.com>
2019-04-16 23:02:33 -0700

    added total spent method

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -3,4 +3,8 @@ class Passenger < ApplicationRecord
 
   validates :name, presence: true
   validates :phone_num, presence: true
+
+  def total_spent
+    return "$%0.2f" % [self.trips.sum { |trip| trip.cost }]
+  end
 end
