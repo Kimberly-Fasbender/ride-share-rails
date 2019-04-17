0455962c7f5da683c164f02e4219474468f3eef7
qqdipps <sdipp007@gmail.com>
2019-04-16 17:54:08 -0700

    finished feature average rating, still in test phase, updated feature next_avail... to return "least recent updated" ->  available driver

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -2,6 +2,15 @@ class Driver < ApplicationRecord
   has_many :trips
 
   def self.next_available
-    return Driver.find_by(available: true)
+    return Driver.where(available: true).order(:updated_at).reverse_order
+  end
+
+  def average_rating
+    rated_trips = trips.where.not(rating: nil)
+    return if rated_trips.empty?
+    sum = trips.sum do |trip|
+      trip.rating if trip.rating
+    end
+    return sum / rated_trips.count.to_f
   end
 end
