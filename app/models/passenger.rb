ea4dd7bbacd74da1ffcd14e9b61a2a549668e090
qqdipps <sdipp007@gmail.com>
2019-04-18 00:19:02 -0700

    passenger can not request trip while trip in progress

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -7,4 +7,8 @@ class Passenger < ApplicationRecord
   def total_spent
     return "$%0.2f" % [self.trips.sum { |trip| trip.cost }]
   end
+
+  def trip_in_progress
+    return self.trips.find { |trip| trip.rating.nil? }
+  end
 end
