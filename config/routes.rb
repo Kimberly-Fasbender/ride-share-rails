17c0759ff4217b9528c712c7a347dd3a59ac4ac5
qqdipps <sdipp007@gmail.com>
2019-04-17 20:09:27 -0700

    removed extra routes for trips

--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,5 @@ Rails.application.routes.draw do
   resources :drivers
   patch "/drivers/:id/availability", to: "drivers#availability", as: "availability"
 
-  resources :trips
+  resources :trips, except: [:new, :index]
 end
