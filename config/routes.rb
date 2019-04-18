33441a5456581f2383020e1386c8bd1f388c2bce
qqdipps <sdipp007@gmail.com>
2019-04-17 20:15:30 -0700

    added nested route for trip passenger

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,9 @@ Rails.application.routes.draw do
 
   root to: "drivers#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :passengers
+  resources :passengers do
+    resources :trips, only: :create
+  end
 
   resources :drivers
   patch "/drivers/:id/availability", to: "drivers#availability", as: "availability"
