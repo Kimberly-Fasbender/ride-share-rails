c134a0a9055c20ac7a06ccbe52f8295c5c1e3743
qqdipps <sdipp007@gmail.com>
2019-04-18 20:00:45 -0700

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,13 @@
 Rails.application.routes.draw do
-  get "trips/index"
-  get "trips/show"
-  get "trips/create"
-  get "trips/new"
-  get "trips/update"
-  get "trips/edit"
-  get "trips/destroy"
-
   root to: "drivers#index"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :trips, except: [:new, :index]
+  patch "/trips/:id/complete", to: "trips#complete", as: "complete_trip"
+
   resources :passengers do
-    resources :trips, only: :create
+    resources :trips, only: [:create]
   end
-  get "/passengers/:id/trips", to: "passengers#show"
 
   resources :drivers
   patch "/drivers/:id/availability", to: "drivers#availability", as: "availability"
-
-  resources :trips, except: [:new, :index]
 end
