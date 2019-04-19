aa223291325597a75753d5f40f26ce6618c0cfa1
Kimberly-Fasbender <kimberly.fasbender@gmail.com>
2019-04-18 20:15:44 -0700

    updated routes file to include homepages show action

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,9 @@
 Rails.application.routes.draw do
-  get "trips/index"
-  get "trips/show"
-  get "trips/create"
-  get "trips/new"
-  get "trips/update"
-  get "trips/edit"
-  get "trips/destroy"
+  root to: "homepages#show"
 
-  root to: "drivers#index"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :passengers do
     resources :trips, only: :create
   end
-  get "/passengers/:id/trips", to: "passengers#show"
 
   resources :drivers
   patch "/drivers/:id/availability", to: "drivers#availability", as: "availability"
